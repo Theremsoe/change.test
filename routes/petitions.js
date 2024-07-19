c68d3627f73fa0a481191d26bb61a57f96d74240
Julio Jaramillo <juliojaramillo@192.168.1.4>
2024-07-18 19:24:00 -0600

    feat: petitions endpoint
    improved and included petitions endpoints
    
     - updated petitions endpoint to return the last 3 records based on created_at criteria
     - added feed endpoint to return petitions based on relationships and updated_at criteria

--- routes/petitions.js
+++ routes/petitions.js
@@ -1,10 +1,62 @@
 const jsonPromiseHandler = require('../lib/jsonPromiseHandler');
 const { runQuery } = require('../lib/sql');
 
+/**
+ *
+ * Get petitions
+ *
+ * Reads petitions entity and return the 3 most recently created petitions from the past week
+ * @param {Object} app
+ * @returns {Record<String, unknown>}
+ */
 async function getPetitions(app) {
-    return runQuery(app.db, 'SELECT * FROM petitions;');
+    return runQuery(
+        app.db,
+        `SELECT * FROM
+            petitions
+        WHERE
+            petitions.created_at BETWEEN DATE('now', 'weekday 0', '-15 days') AND DATE('now', 'weekday 0', '-7 days')
+        ORDER BY
+            created_at ASC
+        LIMIT 3`
+    );
+}
+
+/**
+ * Get petitions for newsfeed
+ *
+ * Returns a list of petitions from starters that you follow ordered by most recently updated
+ * @param {Object} app
+ * @returns {Record<String, unknown>}
+ */
+async function getPetitionsNewsFeed(app, req) {
+    const { id } = req.params;
+    const result = [];
+
+    const stmt = app.db.prepare(`
+        SELECT petitions.* FROM
+            follows
+            INNER JOIN petitions ON follows.starter_urn = petitions.starter_urn
+        WHERE
+            follows.user_id = :user
+        ORDER BY
+            petitions.created_at DESC;
+    `);
+
+    stmt.bind({ ':user': id });
+
+    while (stmt.step()) {
+        const row = stmt.getAsObject();
+        result.push(row);
+    }
+
+    return result;
 }
 
 module.exports = (app) => {
     app.get('/petitions', jsonPromiseHandler(getPetitions, app));
+    app.get(
+        '/petitions/:id/feed',
+        jsonPromiseHandler(getPetitionsNewsFeed, app)
+    );
 };
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const app = require('./app');
 const config = require('./config.json');
 const { getConnection } = require('./lib/sql');
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+});
+
 app.listen(config.port, async () => {
     app.db = await getConnection(`${__dirname}/db/node_api_exercise.sqlite`);
     console.log(`node_api_exercise app listening on port ${config.port}`);
